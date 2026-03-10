171fa80ae826ee4fa0a9fcd6fd027dd2ab74904b
Sven Schroeder <schuen@gmx.de>
2026-03-10 19:29:31 +0100

    add some comments

--- 02/guessing_game/src/main.rs
+++ 02/guessing_game/src/main.rs
@@ -7,9 +7,11 @@ fn main() {
     println!("Please input your guess.");
 
     // a mutable variable of type String
+    // :: means it's an associated function, new() is an associated function of teh String type
     let mut guess = String::new();
-
+    // calls the stdin() function from the io module
     io::stdin()
+        // the & indicates that the argument is a reference &mut means a mutable reference
         .read_line(&mut guess)
         .expect("Failed to read line");
 
