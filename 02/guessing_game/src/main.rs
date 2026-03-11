b3147804660395bd1d88d047abba360bb92cd6e9
Sven Schroeder <schuen@gmx>
2026-03-11 11:55:05 +0100

    add Rng

--- 02/guessing_game/src/main.rs
+++ 02/guessing_game/src/main.rs
@@ -1,9 +1,13 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
 
     // println! is macro - a feature that allows to write code to generate other code
-    println!("Guess the number");
+    println!("Guess the number!");
+
+    let secret_number = rand::rng().random_range(1..=100);
+    println!("The random number is {secret_number}");
     println!("Please input your guess.");
 
     // a mutable variable of type String
@@ -12,6 +16,8 @@ fn main() {
     // calls the stdin() function from the io module
     io::stdin()
         // the & indicates that the argument is a reference &mut means a mutable reference
+        // like variables, references are inmutable by default
+        // Result is an enum that can be either Ok or Err, and expect() is a method that returns the value inside the Ok variant or panics if it's an Err
         .read_line(&mut guess)
         .expect("Failed to read line");
 
