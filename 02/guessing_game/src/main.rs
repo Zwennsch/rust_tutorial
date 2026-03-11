f72942d9d3d549784104272c8452506577b5f236
Sven Schroeder <schuen@gmx.de>
2026-03-11 14:45:31 +0100

    add match statement

--- 02/guessing_game/src/main.rs
+++ 02/guessing_game/src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
 use rand::Rng;
+// the ordering type is an enum with values Less, Greater and Equal
+use std::cmp::Ordering;
 
 fn main() {
 
@@ -16,11 +18,22 @@ fn main() {
     // calls the stdin() function from the io module
     io::stdin()
         // the & indicates that the argument is a reference &mut means a mutable reference
-        // like variables, references are inmutable by default
+        // like variables, references are immutable by default
         // Result is an enum that can be either Ok or Err, and expect() is a method that returns the value inside the Ok variant or panics if it's an Err
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    // This shadows the previous variable guess
+    // We have to use the trim(), otherwise the "Enter" i.e. \r\n" will not be trimmed from the String
+    // The pase() method knows the type in which to convert to since we gave the guess variable a specific type when declaring
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Equal => println!("You win!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Less => println!("Too small!"),
+    }
     
 }
