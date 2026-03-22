9164ebe906817df9d5c7204a877813b06758804a
Sven Schroeder <schuen@gmx.de>
2026-03-22 23:19:55 +0100

    finish game

--- 02/guessing_game/src/main.rs
+++ 02/guessing_game/src/main.rs
@@ -9,31 +9,42 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::rng().random_range(1..=100);
-    println!("The random number is {secret_number}");
-    println!("Please input your guess.");
+    // println!("The random number is {secret_number}");
 
-    // a mutable variable of type String
-    // :: means it's an associated function, new() is an associated function of teh String type
-    let mut guess = String::new();
-    // calls the stdin() function from the io module
-    io::stdin()
-        // the & indicates that the argument is a reference &mut means a mutable reference
-        // like variables, references are immutable by default
-        // Result is an enum that can be either Ok or Err, and expect() is a method that returns the value inside the Ok variant or panics if it's an Err
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    // This shadows the previous variable guess
-    // We have to use the trim(), otherwise the "Enter" i.e. \r\n" will not be trimmed from the String
-    // The pase() method knows the type in which to convert to since we gave the guess variable a specific type when declaring
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Less => println!("Too small!"),
+    loop {
+        println!("Please input your guess.");
+    
+        // a mutable variable of type String
+        // :: means it's an associated function, new() is an associated function of teh String type
+        let mut guess = String::new();
+        // calls the stdin() function from the io module
+        io::stdin()
+            // the & indicates that the argument is a reference &mut means a mutable reference
+            // like variables, references are immutable by default
+            // Result is an enum that can be either Ok or Err, and expect() is a method that returns the value inside the Ok variant or panics if it's an Err
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        // This shadows the previous variable guess
+        // We have to use the trim(), otherwise the "Enter" i.e. \r\n" will not be trimmed from the String
+        // The pase() method knows the type in which to convert to since we gave the guess variable a specific type when declaring
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            // Continue means: go the next iteration of the loop, so immediately to the beginning of the loop!
+            Err(_) => continue,
+        };
+    
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Equal =>{
+                println!("You win!");
+                break;
+            } 
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small!"),
+        }
+        
     }
     
 }
