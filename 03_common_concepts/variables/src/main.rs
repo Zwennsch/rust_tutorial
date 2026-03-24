b7c15b2b183594e7f26e461093e3211a9908e634
Sven Schroeder <schuen@gmx.de>
2026-03-24 11:37:35 +0100

    add 3_1 variables

---
+++ 03_common_concepts/variables/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+   let x = 5;
+   println!("the value of x is:{x}");
+//    This won't work since variables are immutable by default:
+//    x = 6;
+   println!("The value of x is: {x}");
+}
