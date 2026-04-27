76488283222c032a23b0f92e7151f86a5f5dc275
Sven Schroeder <schuen@gmx.de>
2026-04-27 09:13:16 +0200

    finish variables and shadowing

--- 03_common_concepts/variables/src/main.rs
+++ 03_common_concepts/variables/src/main.rs
@@ -3,5 +3,45 @@ fn main() {
    println!("the value of x is:{x}");
 //    This won't work since variables are immutable by default:
 //    x = 6;
+
+// What you can do is this:
+let x = x+ 1;
+// This is called shadowing
    println!("The value of x is: {x}");
+
+   fn test(){
+      println!("This is inside the test inside main");
+      // This is not working. I cannot use x inside an inner function
+      // let x = x*2;
+      // not possible: cannot access from inside an inner function but inner scope is OK (see below)
+      // println!("Can I use the x variable from main= {x}");
+   }
+   {
+      // But I can do this:
+      let x = x*2;
+      println!("The value of the inner scope x is : {x}")
+   }
+   test();
+   println!("The value of x after running test function: {x}")
 }
+
+/*
+Very important:
+In Rust a function fn ... and a scope {...} are totally different things
+A block is a nested lexical scope. The Block can "see" the variables from the outer scope
+Functions in Rust do not capture variables from its parent scope
+Functions are independent items in Rust!!
+Functions in Rust are compile-time items not runtime closures
+Functions have to be explicit and predictable 
+If you want to use outer variables you have to use closures like this:
+fn main() {
+    let x = 6;
+
+    let test = || {
+        let x = x * 2;
+        println!("Inside closure: {x}");
+    };
+
+    test();
+}
+ */
